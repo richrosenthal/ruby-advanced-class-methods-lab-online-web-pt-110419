856a4a255c08d452003e969d3ff02092c9e9bfcd
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-23 03:00:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,73 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+   song = self.new
+   @@all << song
+   song 
+  end 
 
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name 
+    song
+  end 
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song 
+    song
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find{|song_name| song_name.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    if !find_by_name(name)
+      create_by_name(name)
+    else
+      find_by_name(name)
+    end
+  end 
+  
+  def self.alphabetical 
+    @@all.sort_by { |song| song.name }
+  end 
+  
+  def self.new_from_filename(file_name)
+    string = file_name.split(".")
+    file_name_string = string[0]
+    data = file_name_string.split(" - ")
+    artist_name = data[0]
+    song_name = data[1]
+    
+    song = self.new 
+    song.artist_name = artist_name
+    song.name = song_name
+    song
+  end
+  
+  def self.create_from_filename(file_name)
+    string = file_name.split(".")
+    file_name_string = string[0]
+    data = file_name_string.split(" - ")
+    artist_name = data[0]
+    song_name = data[1]
+    
+    song = self.new 
+    song.artist_name = artist_name
+    song.name = song_name
+    @@all << song 
+    song
+  end
+  
+  def self.destroy_all 
+    @@all.clear
+  end 
+  
+
+  
 end
